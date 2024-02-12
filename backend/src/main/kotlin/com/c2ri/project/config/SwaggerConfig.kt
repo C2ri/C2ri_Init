e0a2fca3edeb75be99a7a014ba9e4298190c311b
devyoseph <josephdev123@naver.com>
2024-02-12 21:03:15 +0900

    Feat : Spring3-Swagger 호환성 문제로 라이브러리 교체 (#4)

--- backend/src/main/kotlin/com/c2ri/project/config/SwaggerConfig.kt
+++ backend/src/main/kotlin/com/c2ri/project/config/SwaggerConfig.kt
@@ -3,11 +3,10 @@ package com.c2ri.project.config
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import lombok.RequiredArgsConstructor
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 
-//
+//localhost:8080/swagger-ui/index.html
 @OpenAPIDefinition(
     info = Info(title       = "셔를레이호 App",
                 description = "셔를레이호 API 명세",
